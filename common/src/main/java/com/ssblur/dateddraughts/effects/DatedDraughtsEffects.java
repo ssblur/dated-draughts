294821b80f1aa57f956e7e13e259b54a5545eeb6
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-11-02 20:50:07 -0400

    Add Potions

--- common/src/main/java/com/ssblur/dateddraughts/effects/DatedDraughtsEffects.java
+++ common/src/main/java/com/ssblur/dateddraughts/effects/DatedDraughtsEffects.java
@@ -7,8 +7,11 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.item.alchemy.Potion;
 import org.jetbrains.annotations.Nullable;
 
+@SuppressWarnings("unused")
 public class DatedDraughtsEffects {
   public static final DeferredRegister<MobEffect> EFFECTS = DeferredRegister.create(DatedDraughts.MOD_ID, Registries.MOB_EFFECT);
   public static final RegistrySupplier<MobEffect> BETA_FOOD = EFFECTS.register("beta_food",
@@ -18,9 +21,25 @@ public class DatedDraughtsEffects {
   public static final RegistrySupplier<MobEffect> OLD_COMBAT = EFFECTS.register("old_combat",
     () -> new DatedDraughtEffect(MobEffectCategory.NEUTRAL, 0xffff0000));
 
+  public static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(DatedDraughts.MOD_ID, Registries.POTION);
+  public static final RegistrySupplier<Potion> BETA_FOOD_POTION = POTIONS.register("beta_food",
+    () -> new Potion("beta_food", new MobEffectInstance(BETA_FOOD, 300)));
+  public static final RegistrySupplier<Potion> OLD_WATER_POTION = POTIONS.register("old_water",
+    () -> new Potion("old_water", new MobEffectInstance(OLD_WATER, 300)));
+  public static final RegistrySupplier<Potion> OLD_COMBAT_POTION = POTIONS.register("old_combat",
+    () -> new Potion("old_combat", new MobEffectInstance(OLD_COMBAT, 300)));
+  public static final RegistrySupplier<Potion> DATED_DRAUGHT_POTION = POTIONS.register("dated_draught",
+    () -> new Potion(
+      "dated_draught",
+      new MobEffectInstance(OLD_COMBAT, 300),
+      new MobEffectInstance(OLD_WATER, 300),
+      new MobEffectInstance(BETA_FOOD, 300)
+    )
+  );
 
   public static void init() {
     EFFECTS.register();
+    POTIONS.register();
   }
 
   @Nullable
