9a9abaa762ef27f5f34a0449456df1dd620f5872
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-11-05 12:16:07 -0500

    Localize Splash and Lingering potions

--- common/src/main/java/com/ssblur/dateddraughts/item/DatedDraughtsItems.java
+++ common/src/main/java/com/ssblur/dateddraughts/item/DatedDraughtsItems.java
@@ -31,6 +31,16 @@ public class DatedDraughtsItems {
             output.accept(PotionContents.createItemStack(Items.POTION, DatedDraughtsEffects.OLD_WATER_POTION));
             output.accept(PotionContents.createItemStack(Items.POTION, DatedDraughtsEffects.BETA_FOOD_POTION));
             output.accept(PotionContents.createItemStack(Items.POTION, DatedDraughtsEffects.DATED_DRAUGHT_POTION));
+
+            output.accept(PotionContents.createItemStack(Items.SPLASH_POTION, DatedDraughtsEffects.OLD_COMBAT_POTION));
+            output.accept(PotionContents.createItemStack(Items.SPLASH_POTION, DatedDraughtsEffects.OLD_WATER_POTION));
+            output.accept(PotionContents.createItemStack(Items.SPLASH_POTION, DatedDraughtsEffects.BETA_FOOD_POTION));
+            output.accept(PotionContents.createItemStack(Items.SPLASH_POTION, DatedDraughtsEffects.DATED_DRAUGHT_POTION));
+
+            output.accept(PotionContents.createItemStack(Items.LINGERING_POTION, DatedDraughtsEffects.OLD_COMBAT_POTION));
+            output.accept(PotionContents.createItemStack(Items.LINGERING_POTION, DatedDraughtsEffects.OLD_WATER_POTION));
+            output.accept(PotionContents.createItemStack(Items.LINGERING_POTION, DatedDraughtsEffects.BETA_FOOD_POTION));
+            output.accept(PotionContents.createItemStack(Items.LINGERING_POTION, DatedDraughtsEffects.DATED_DRAUGHT_POTION));
           });
         }
       )
